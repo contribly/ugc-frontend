2b096c580b462347e2069caec8357546a3eae11b
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-10-26 08:14:09 +0000

    Login form.

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -1,6 +1,10 @@
 
 package controllers
 
+import play.api.Logger
+import play.api.data.Forms._
+import play.api.data._
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -10,14 +14,37 @@ object LoginController extends Controller with PageSize {
 
   val signedInUserService = SignedInUserService
 
-  def login(username: String, password: String) = Action.async {request =>
-    val token: Future[Option[String]] = signedInUserService.signin(username, password, request)
+  val loginForm: Form[(String, String)] = Form(
+    tuple(
+      "username" -> text,
+      "password" -> text
+    )
+  )
 
-    token.map(to => {
-      to.fold(Redirect(routes.Application.index()))(t =>
-        Redirect(routes.Application.index()).withSession("token" -> t)
-      )
-    })
+  def prompt() = Action.async {
+    Future.successful(Ok(views.html.login(loginForm)))
+  }
+
+  def submit() = Action.async { request =>
+
+    val boundForm: Form[(String, String)] = loginForm.bindFromRequest()(request)
+
+    boundForm.fold(
+      formWithErrors => {
+        Future.successful(BadRequest(Json.toJson("Bad form data: " + formWithErrors.errors)))
+      },
+      userData => {
+        val eventualMaybeToken: Future[Option[String]] = signedInUserService.signin(userData._1, userData._2, request)
+        eventualMaybeToken.map(to => {
+          to.fold(
+            BadRequest(Json.toJson("No token"))
+          )(t => {
+            Logger.info("Setting session token: " + t)
+            Redirect(routes.Application.index()).withSession("token" -> t)
+          })
+        })
+      }
+    )
   }
 
   def logout = Action {
