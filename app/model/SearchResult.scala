acec8fb9f25c0a5102a5f1cae999cb179183f7d2
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-11-26 18:27:53 +0000

    Map noticeboard moderated and featured flags

--- app/model/SearchResult.scala
+++ app/model/SearchResult.scala
@@ -85,7 +85,8 @@ object Place {
 
 case class Noticeboard(val id: String, val name: String, val description: Option[String],
                         val geoCodingResolution:  Option[String],
-                        val endDate: Option[DateTime], val embargoDate: Option[DateTime], val scheduledDate: Option[DateTime])
+                        val endDate: Option[DateTime], val embargoDate: Option[DateTime], val scheduledDate: Option[DateTime],
+                        val moderated: Boolean, val featured: Boolean)
 
 object Noticeboard {
   implicit val df: Reads[DateTime] = DateTimeFormat
