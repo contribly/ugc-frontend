954304fd16fc992932ea988498d0736938a0165c
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-02-19 16:52:30 +0000

    Clean up. Alphabetical order.

--- app/model/Model.scala
+++ app/model/Model.scala
@@ -3,7 +3,7 @@ package model
 import java.util.Date
 
 import org.joda.time.DateTime
-import org.joda.time.format.{ISODateTimeFormat, DateTimeFormatter}
+import org.joda.time.format.ISODateTimeFormat
 import play.api.libs.json._
 
 class DateTimeFormat extends Reads[DateTime] {
@@ -19,6 +19,12 @@ class DateTimeFormat extends Reads[DateTime] {
 }
 
 object DateTimeFormat extends DateTimeFormat
+
+case class Artifact(val contentType: String, val url: Option[String], val width: Option[Int], val height: Option[Int], val label: String)
+
+object Artifact {
+  implicit val formats: Format[Artifact] = Json.format[Artifact]
+}
 
 case class Authority(val client: Option[Client])
 
@@ -36,33 +42,6 @@ case class FlagType(val id: String, val name: String)
 
 object FlagType {
   implicit val formats: Format[FlagType] = Json.format[FlagType]
-}
-
-case class SearchResult(val numberFound: Long, val startIndex: Long, results: Seq[Report])
-
-object SearchResult {
-  implicit val formats: Format[SearchResult] = Json.format[SearchResult]
-}
-
-case class Artifact(val contentType: String, val url: Option[String], val width: Option[Int], val height: Option[Int], val label: String)
-
-object Artifact {
-  implicit val formats: Format[Artifact] = Json.format[Artifact]
-}
-
-case class Report(val id: String, val headline: String, created: DateTime, noticeboard: Option[Noticeboard],
-                  user: User, body: Option[String], tags: Seq[Tag], place: Option[Place],
-                  mediaUsages: Seq[MediaUsage], via: Option[Authority])
-
-object Report {
-  implicit val df: Reads[DateTime] = DateTimeFormat
-  implicit val formats: Format[Report] = Json.format[Report]
-}
-
-case class User(val id: String, val displayName: String, registered: Option[Date], via: Option[Authority])
-
-object User {
-  implicit val formats: Format[User] = Json.format[User]
 }
 
 case class LatLong(val latitude: Double, val longitude: Double)
@@ -83,30 +62,12 @@ object MediaUsage {
   implicit val formats: Format[MediaUsage] = Json.format[MediaUsage]
 }
 
-case class Osm(val osmId: Long, osmType: String)
-
-object Osm {
-  implicit val formats: Format[Osm] = Json.format[Osm]
-}
-
-case class Tag(val id: String, val name: String)
-
-object Tag {
-  implicit val formats: Format[Tag] = Json.format[Tag]
-}
-
-case class Place(val name: Option[String], val latLong: Option[LatLong], val osm: Option[Osm])
-
-object Place {
-  implicit val formats: Format[Place] = Json.format[Place]
-}
-
 case class Noticeboard(val id: String, val name: String, val description: Option[String],
-                        val geoCodingResolution:  Option[String],
-                        val endDate: Option[DateTime], val embargoDate: Option[DateTime], val scheduledDate: Option[DateTime],
-                        val moderated: Boolean, val featured: Boolean, val cover: Option[Media],
-                        val supportedMediaTypes: Set[String],
-                        val contributions: Option[Int])
+                       val geoCodingResolution:  Option[String],
+                       val endDate: Option[DateTime], val embargoDate: Option[DateTime], val scheduledDate: Option[DateTime],
+                       val moderated: Boolean, val featured: Boolean, val cover: Option[Media],
+                       val supportedMediaTypes: Set[String],
+                       val contributions: Option[Int])
 
 object Noticeboard {
   implicit val df: Reads[DateTime] = DateTimeFormat
@@ -117,4 +78,43 @@ case class NoticeboardSearchResult(val numberFound: Long, results: Seq[Noticeboa
 
 object NoticeboardSearchResult {
   implicit val formats: Format[NoticeboardSearchResult] = Json.format[NoticeboardSearchResult]
+}
+
+case class Report(val id: String, val headline: String, created: DateTime, noticeboard: Option[Noticeboard],
+                  user: User, body: Option[String], tags: Seq[Tag], place: Option[Place],
+                  mediaUsages: Seq[MediaUsage], via: Option[Authority])
+
+case class Osm(val osmId: Long, osmType: String)
+
+object Osm {
+  implicit val formats: Format[Osm] = Json.format[Osm]
+}
+
+case class Place(val name: Option[String], val latLong: Option[LatLong], val osm: Option[Osm])
+
+object Place {
+  implicit val formats: Format[Place] = Json.format[Place]
+}
+
+object Report {
+  implicit val df: Reads[DateTime] = DateTimeFormat
+  implicit val formats: Format[Report] = Json.format[Report]
+}
+
+case class SearchResult(val numberFound: Long, val startIndex: Long, results: Seq[Report])
+
+object SearchResult {
+  implicit val formats: Format[SearchResult] = Json.format[SearchResult]
+}
+
+case class Tag(val id: String, val name: String)
+
+object Tag {
+  implicit val formats: Format[Tag] = Json.format[Tag]
+}
+
+case class User(val id: String, val displayName: String, registered: Option[Date], via: Option[Authority])
+
+object User {
+  implicit val formats: Format[User] = Json.format[User]
 }
