fb6fda6ed764b4e7c931f40960cf1c6f5d8ab6ac
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-02-26 10:14:29 +0000

    Can obtain Twitter access token. Need to exchange for an API token.

--- app/controllers/TwitterLoginController.scala
+++ app/controllers/TwitterLoginController.scala
@@ -1,9 +1,11 @@
 package controllers
 
 import _root_.twitter4j.conf.{Configuration, ConfigurationBuilder}
+import akka.serialization.Serialization
+import play.api.libs.json.Json
 import twitter4j.{TwitterException, Twitter, TwitterFactory}
 import play.api.Play.current
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Session, Action, Controller}
 import play.api.{Logger, Play}
 import services.ugc.UGCService
 import twitter4j.auth.{AccessToken, RequestToken}
@@ -20,45 +22,56 @@ trait TwitterLoginController extends Controller {
   val consumerSecret: String
 
   val callbackUrl = rootUrl + routes.TwitterLoginController.callback(None, None)
+  val TwitterRequestTokenSessionKey: String = "twitter-request-token"
 
   def redirect() = Action.async { request =>
 
     val twitterApi = getTwitterApi
 
     val requestToken: RequestToken = twitterApi.getOAuthRequestToken(callbackUrl)
-    //requestTokens.put(requestToken.getToken, requestToken)  TODO need state
+    val serializedRequestToken = Json.toJson(Map(
+      "token" -> requestToken.getToken,
+      "secret" -> requestToken.getTokenSecret
+    )
+    ).toString()
+
     val loginDialogUrlString = requestToken.getAuthenticationURL
 
     Logger.info("Redirecting to Twitter login dialog: " + loginDialogUrlString)
-    Future.successful(Redirect(loginDialogUrlString))
+    val withRequestToken: Session = request.session + (TwitterRequestTokenSessionKey, serializedRequestToken)
+    Future.successful(Redirect(loginDialogUrlString).withSession(withRequestToken))
   }
 
   def callback(oauth_token: Option[String], oauth_verifier: Option[String]) = Action.async { request =>
-      Logger.info("Recieved Twitter callback: " + oauth_token + ", " + oauth_verifier)
+      Logger.info("Received Twitter callback: " + oauth_token + ", " + oauth_verifier)
 
       oauth_token.map { t =>
         oauth_verifier.map { v =>
 
-          val twitterApi: Twitter = getTwitterApi
+          request.session.get(TwitterRequestTokenSessionKey).map { srt =>
+            val rtMap = Json.parse(srt).as[Map[String, String]]
+            val requestToken = new RequestToken(rtMap.get("token").getOrElse(""), rtMap.get("secret").getOrElse(""))
+            try {
+              Logger.info("Exchanging Twitter request token for access token: " + requestToken + ", " + v)
+              val twitterApi: Twitter = getTwitterApi
+              val twitterAccessToken: AccessToken = twitterApi.getOAuthAccessToken(requestToken, v)
+              Logger.info("Got Twitter access token: " + twitterAccessToken.getToken)
 
-          val requestToken: RequestToken = twitterApi.getOAuthRequestToken(callbackUrl)
-
-          try {
-            val twitterAccessToken: AccessToken = twitterApi.getOAuthAccessToken(requestToken, v)
-            Logger.info("Got access token: " + twitterAccessToken.getToken)
-
-          } catch {
-            case te: TwitterException => {
-              Logger.warn("Twitter exception while trying to exchange for access token", te)
-            }
-            case e: Exception => {
-              Logger.error("Exception while trying to exchange for access token", e)
+            } catch {
+              case te: TwitterException => {
+                Logger.warn("Twitter exception while trying to exchange for access token", te)
+              }
+              case e: Exception => {
+                Logger.error("Exception while trying to exchange for access token", e)
+              }
             }
           }
         }
       }
 
-      Future.successful(Redirect(routes.Application.index(None, None)))   // TODO implement
+    val withClearedRequestToken: Session = request.session - (TwitterRequestTokenSessionKey)
+
+    Future.successful(Redirect(routes.Application.index(None, None)).withSession(withClearedRequestToken))
   }
 
   private def getTwitterApi: Twitter = {
