9cea2e0096e6d997d018cf20fc498be7b2ea286d
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-02-26 09:44:38 +0000

    Stop and thing about where to store the request token in a Play app.
    As the session is stored in the user's cookie which goes over the wire. Does this defeat the propose of the request token and allow MitM attacks?

--- app/controllers/TwitterLoginController.scala
+++ app/controllers/TwitterLoginController.scala
@@ -1,12 +1,12 @@
 package controllers
 
 import _root_.twitter4j.conf.{Configuration, ConfigurationBuilder}
-import _root_.twitter4j.{Twitter, TwitterFactory}
+import twitter4j.{TwitterException, Twitter, TwitterFactory}
 import play.api.Play.current
 import play.api.mvc.{Action, Controller}
 import play.api.{Logger, Play}
 import services.ugc.UGCService
-import twitter4j.auth.RequestToken
+import twitter4j.auth.{AccessToken, RequestToken}
 
 import scala.concurrent.Future
 
@@ -19,17 +19,9 @@ trait TwitterLoginController extends Controller {
   val consumerKey: String
   val consumerSecret: String
 
-  val callbackUrl = rootUrl + routes.FacebookLoginController.callback(None, None, None, None)
+  val callbackUrl = rootUrl + routes.TwitterLoginController.callback(None, None)
 
   def redirect() = Action.async { request =>
-
-    def getTwitterApi: Twitter = {
-      return new TwitterFactory(buildConfig).getInstance
-    }
-
-    def buildConfig: Configuration = {
-      new ConfigurationBuilder().setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret).build
-    }
 
     val twitterApi = getTwitterApi
 
@@ -42,7 +34,38 @@ trait TwitterLoginController extends Controller {
   }
 
   def callback(oauth_token: Option[String], oauth_verifier: Option[String]) = Action.async { request =>
+      Logger.info("Recieved Twitter callback: " + oauth_token + ", " + oauth_verifier)
+
+      oauth_token.map { t =>
+        oauth_verifier.map { v =>
+
+          val twitterApi: Twitter = getTwitterApi
+
+          val requestToken: RequestToken = twitterApi.getOAuthRequestToken(callbackUrl)
+
+          try {
+            val twitterAccessToken: AccessToken = twitterApi.getOAuthAccessToken(requestToken, v)
+            Logger.info("Got access token: " + twitterAccessToken.getToken)
+
+          } catch {
+            case te: TwitterException => {
+              Logger.warn("Twitter exception while trying to exchange for access token", te)
+            }
+            case e: Exception => {
+              Logger.error("Exception while trying to exchange for access token", e)
+            }
+          }
+        }
+      }
+
       Future.successful(Redirect(routes.Application.index(None, None)))   // TODO implement
+  }
+
+  private def getTwitterApi: Twitter = {
+    def buildConfig: Configuration = {
+      new ConfigurationBuilder().setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret).build
+    }
+    return new TwitterFactory(buildConfig).getInstance
   }
 
 }
