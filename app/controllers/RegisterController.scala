08e9c6cd76ae69e51872a6cd53f18248713db965
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-05-12 08:54:57 +0100

    Clean up - user owner wrapper.

--- app/controllers/RegisterController.scala
+++ app/controllers/RegisterController.scala
@@ -1,16 +1,17 @@
 package controllers
 
+import model.User
 import model.forms.RegistrationDetails
 import play.api.Logger
 import play.api.data.Forms._
 import play.api.data._
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Result, Request, Action, Controller}
 import services.ugc.UGCService
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-object RegisterController extends Controller {
+object RegisterController extends Controller with WithOwner {
 
   val signedInUserService = SignedInUserService
   val ugcService = UGCService
@@ -23,58 +24,51 @@ object RegisterController extends Controller {
   )
 
   def prompt() = Action.async { request =>
-    for {
-      owner <- ugcService.owner
-    } yield {
-      owner.fold(NotFound(views.html.notFound())) { o =>
-        val withErrors = request.session.get("error").fold(registrationForm){ e =>
-          registrationForm.withGlobalError(e)
-        }
-        Ok(views.html.register(registrationForm, o)).withSession(request.session - "error")
+
+    val registerPromptPage: (Request[Any], User) => Future[Result] = (request: Request[Any], owner: User) => {
+      val withErrors = request.session.get("error").fold(registrationForm) { e =>
+        registrationForm.withGlobalError(e)
       }
+      Future.successful(Ok(views.html.register(registrationForm, owner)).withSession(request.session - "error"))
     }
+
+    withOwner(request, registerPromptPage)
   }
 
   def submit() = Action.async { request =>
 
-    val eventualOwner = ugcService.owner
+    val registerSubmit: (Request[Any], User) => Future[Result] = (request: Request[Any], owner: User) => {
 
-    eventualOwner.flatMap { owner =>
+      registrationForm.bindFromRequest()(request).fold(
+        formWithErrors => {
+          Future.successful(Ok(views.html.register(formWithErrors, owner)))
+        },
+        registrationDetails => {
+          Logger.info("Attempting to register user: " + registrationDetails)
+          ugcService.register(registrationDetails).flatMap { mu =>
 
-      owner.fold {
-        Logger.warn("Invalid owner; returning 404")
-        Future.successful(NotFound(views.html.notFound()))
-      } { o =>
-
-        registrationForm.bindFromRequest()(request).fold(
-          formWithErrors => {
-            Future.successful(Ok(views.html.register(formWithErrors, o)))
-          },
-          registrationDetails => {
-            Logger.info("Attempting to register user: " + registrationDetails)
-            ugcService.register(registrationDetails).flatMap { mu =>
-
-              mu.fold ( { e =>
-                Logger.warn("Failed to register user: " + e)
-                val withErrors = request.session + ("error", e)
-                Future.successful(Ok(views.html.register(registrationForm, o)).withSession(withErrors))
-              }, { u =>
-                Logger.info("Registered new user: " + u)
-                ugcService.token(u.username, registrationDetails.password).map { to => // TODO Register end point should provide a token as well
-                  to.fold {
-                    Redirect(routes.Application.index(None, None))
-                  } { t =>
-                    Logger.info("Got token: " + t)
-                    Redirect(routes.Application.index(None, None)).withSession(SignedInUserService.sessionTokenKey -> t)
-                  }
+            mu.fold({ e =>
+              Logger.warn("Failed to register user: " + e)
+              val withErrors = request.session +("error", e)
+              Future.successful(Ok(views.html.register(registrationForm, owner)).withSession(withErrors))
+            }, { u =>
+              Logger.info("Registered new user: " + u)
+              ugcService.token(u.username, registrationDetails.password).map { to => // TODO Register end point should provide a token as well
+                to.fold {
+                  Redirect(routes.Application.index(None, None))
+                } { t =>
+                  Logger.info("Got token: " + t)
+                  Redirect(routes.Application.index(None, None)).withSession(SignedInUserService.sessionTokenKey -> t)
                 }
               }
-              )
             }
+            )
           }
-        )
-      }
+        }
+      )
     }
+
+    withOwner(request, registerSubmit)
   }
 
 }
