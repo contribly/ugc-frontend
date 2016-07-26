5caf196df469ae38bc8b0fa26f5e70ea17d2ba2d
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-07-26 22:26:08 +0100

    Remove references to deprecated assignment moderated and supported media types fields.

--- app/model/Model.scala
+++ app/model/Model.scala
@@ -64,9 +64,7 @@ object MediaUsage {
 
 case class Noticeboard(id: String, name: String, description: Option[String],
                        geoCodingResolution:  Option[String],
-                       endDate: Option[DateTime], embargoDate: Option[DateTime], scheduledDate: Option[DateTime],
-                       moderated: Boolean, featured: Boolean, cover: Option[MediaUsage],
-                       supportedMediaTypes: Set[String])
+                       endDate: Option[DateTime], embargoDate: Option[DateTime], scheduledDate: Option[DateTime], featured: Boolean, cover: Option[MediaUsage])
 
 object Noticeboard {
   implicit val df: Reads[DateTime] = DateTimeFormat
